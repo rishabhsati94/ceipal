8f4de676d9c4145ef0924c3269d415af11cd3a77
Aryan Raj <araj60988@gmail.com>
2021-06-26 17:44:13 +0530

    form validation and login signup authentication using json api

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
-import SignIn from './Component/SignIn';
-import SignUp from './Component/SignUp';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Route } from 'react-router-dom'
-
+import React from "react";
+import SignIn from "./Component/SignIn";
+import SignUp from "./Component/SignUp";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Component/Home";
+import Logout from "./Component/Logout";
 
 function App() {
   return (
-   <>
-   <Route exact path="/">
-      <SignIn/>
-    </Route>
-    <Route exact path="/signup">
-      <SignUp/>
-    </Route>
-   </>
+    <>
+      <Switch>
+        <Route exact path="/">
+          <SignIn />
+        </Route>
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
+
+        <Route path="/home" component={Home}></Route>
+        <Route path="/logout" component={Logout}></Route>
+      </Switch>
+    </>
   );
 }
 
